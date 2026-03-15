8cd0a6f2652b551153ad50965a44cecfb03686a1
NakamuraTakumi <takumi.062757@gmail.com>
2026-03-15 21:10:23 +0900

    # chore: add turborepo example for testing (#60)

---
+++ example/turborepo/packages/ui/src/button.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import type { ReactNode } from "react";
+
+interface ButtonProps {
+  appName: string;
+  children: ReactNode;
+  className?: string;
+}
+
+export const Button = ({ children, className, appName }: ButtonProps) => {
+  return (
+    <button
+      className={className}
+      onClick={() => alert(`Hello from your ${appName} app!`)}
+    >
+      {children}
+    </button>
+  );
+};
